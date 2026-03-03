8542599bb9f3013327a4f150f17a5e1b8916545f
Walter Gonzalez <w.alejandro.g.s@gmail.com>
2026-03-03 17:29:15 -0500

    add metod in Defaul5Task

--- src/main/java/com/defaultpackage/tasks/Default5Task.java
+++ src/main/java/com/defaultpackage/tasks/Default5Task.java
@@ -2,10 +2,16 @@ package com.defaultpackage.tasks;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
 public class Default5Task implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
 
+    }
+
+    public static  Default5Task on(){
+        return instrumented(Default5Task.class);
     }
 }
